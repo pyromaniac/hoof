77da5759c94a77ff132d4015c33a74a6b9ab80c6
pyromaniac <kinwizard@gmail.com>
2011-05-15 19:22:30 +0400

    Added initializer

--- lib/hoof/cli.rb
+++ lib/hoof/cli.rb
@@ -4,17 +4,31 @@ require "hoof"
 
 module Hoof
   class Cli < Thor
+    include Thor::Actions
 
     default_task :start
 
-    desc "start", "Starts hoof daemon"
+    desc 'hoof start', 'Starts hoof daemon'
     def start
       daemon 'start'
     end
 
-    desc "stop", "Stops hoof daemon"
+    desc 'hoof stop', 'Stops hoof daemon'
     def stop
       daemon 'stop'
+    end
+
+    desc 'hoof init [NAME]', 'Initializes hoof for app in current directory'
+    long_desc <<-D
+      Initializes hoof for current directory application.
+      This task creates symlink in ~/.hoof and adds unicorn to
+      your application Gemfile.
+      So do not forget to bundle it again.
+    D
+    def init name = nil
+      name ||= File.basename Dir.getwd
+      create_link File.expand_path(File.join("~/.hoof", name)), '.'
+      append_to_file 'Gemfile', "\ngem 'unicorn'"
     end
 
     #desc "list", "Lists started applications"
